079f5e56077d9bb0f1b72d2edc0229a806cc1024
Maciej Kłonicki <maciek.klonicki@gmail.com>
2021-10-06 22:49:53 +0200

    My Knight is movinggit statusgit status!

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -1,52 +1,41 @@
 package com.company;
 
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-public class GamePanel extends JPanel implements MouseListener {
-
-    private final JButton [][] buttons = new JButton[8][8];
 
 
-    GamePanel () {
-        this.setLayout(new GridLayout(8,8));
-        for (int i = 0 ; i < buttons.length ; i++) {
-            for (int j = 0 ; j < buttons[i].length ; j ++ ) {
-                buttons[i][j] = new JButton();
-                this.add(buttons[i][j]);
-                if ((j % 2 == 0) == (i % 2 == 0)) {
-                    buttons[i][j].setBackground(Color.BLACK);
-                } else buttons[i][j].setBackground(Color.WHITE);
+public class GamePanel  extends JFrame {
+
+    //buttons to place all over the screen
+    public final JButton [][] chessBoardButtons = new JButton[8][8];
+
+    public GamePanel () {
+        Knight knight = new Knight(chessBoardButtons);
+        Container container = getContentPane();
+        container.setLayout(new GridLayout(8,8));
+
+        // Chessboard
+        for (int i = 0 ; i < 8 ; i++) {
+            for (int j = 0 ; j < 8 ; j ++ ) {
+                chessBoardButtons[i][j] = new JButton();
+                if ((i+j)%2 !=0) {
+                    chessBoardButtons[i][j].setBackground(new Color(50,100,100));
+                }
+                container.add(chessBoardButtons[i][j]);
+                chessBoardButtons[i][j].addActionListener(knight);
             }
         }
-    }
+        int row = 7;
+        int col = 1;
+        ImageIcon knightIcon = new ImageIcon("knight.png");
+        chessBoardButtons[row][col].setIcon(knightIcon);
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (e.getSource() == buttons[0]){
-            System.out.println("dupa");
-        }
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-
+        setSize(800,800);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
+
+
